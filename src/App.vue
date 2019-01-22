6af1190c51360c3c80001998e07cf9b67ec88246
jianjun.deng <jianjun.deng@shike.com>
2019-01-22 11:17:01 +0800

    暂存

--- src/App.vue
+++ src/App.vue
@@ -19,5 +19,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  font-size: .12rem;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 const HomeIndex = resolve => require(['@/components/yangguan/Index'], resolve) //官网-首页
 const FeatureProject = resolve => require(['@/components/yangguan/Feature'], resolve) //官网-首页
+const StudyApplication = resolve => require(['@/components/yangguan/StudyApplication'], resolve) //官网-首页
 
 Vue.use(Router)
 export default new Router({
@@ -13,6 +14,8 @@ export default new Router({
         {path: '/',component: HomeIndex,alias:"/home"},
         {path: '/feature',component: FeatureProject},
         // {path: '/feature',component: FeatureProject},
+        {path: '/StudyApplication',component: StudyApplication},
+        
     ]
 })
 
