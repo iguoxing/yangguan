d826a558f3f4d290f42c7e41b21cb9d006e35058
国星 <guoxingyueliang@163.com>
2019-01-27 14:28:40 +0800

    nav

---
+++ src/components/yangguan/base/Banner.vue
@@ -0,0 +1,80 @@
+<template>
+<div class="header">
+  <div class="header-vision">
+      <span class="vision-bigger">Vision</span>[愿景]
+    </div>
+    <div class="header-sub-vision">
+      <p>每一所申请院校都应是你心仪且合适的</p>
+      <p>精细拆分的 <b>8M</b> 单项服务，把钱用在最有价值的服务上</p>
+      <p>服务不止于 <b>offer</b>，更加关注 <b>安全 文化 心理 职业</b> 软性辅导</p>
+    </div>
+    <a class="header-btn">了解更多</a>
+</div>  
+</template>
+<script>
+
+import NavBar from '@/components/yangguan/base/Nav';
+
+export default {
+  data () {
+    return {
+      msg: '洋观',
+    }
+  },
+  components:{
+    NavBar
+  },
+}
+</script>
+<style lang="less">
+.header{
+  width: 100%;
+  height: 7.62rem;
+  box-sizing: border-box;
+  padding-top: 1rem;
+  background: url(https://guanyang.oss-cn-beijing.aliyuncs.com/assets/img/index/header-bg.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  color: #fff;
+}
+.header-vision{
+    margin-top: 1.25rem;
+    margin-bottom: .3rem;
+    font-size: .36rem;
+    .vision-bigger{
+      font-size: .5rem;
+      font-weight: 500;
+    }
+  }
+  .header-sub-vision{
+    font-size: .24rem;
+    p{
+      margin-top: 0;
+      margin-bottom: .12rem;
+      b{
+        font-size: .36rem;
+        font-weight: 400;
+      }
+    }
+  
+  }
+  .header-btn{
+    display: block;
+    margin: .6rem auto;
+    width: 1.8rem;
+    height: .56rem;
+    line-height: .56rem;
+    border: solid .01rem #fff;
+    border-radius: .5rem;
+    font-size: .24rem;
+    color: #fff;
+    cursor: pointer;
+  }
+  .header-btn:hover{
+    font-weight: 500;
+    color: #666;
+    background: #fff;
+  }
+
+
+</style>
--- src/components/yangguan/base/NavClarity.vue
+++ src/components/yangguan/base/NavClarity.vue
@@ -1,34 +1,21 @@
 <template>
-<div class="header-nav second">
+<div class="header-nav first">
   <div class="header-title">
-      <a class="logo">
-        <img src="https://guanyang.oss-cn-beijing.aliyuncs.com/assets/img/common/logo.png">
-      </a>
-      <ul>
-        <li class="active"><a href="/">首页</a></li>
-        <li class="dot"></li>
-        <li><a>留学申请</a></li>
-        <li class="dot"></li>
-        <li><a>单项服务</a></li>
-        <li class="dot"></li>
-        <li><a href="/feature">特色项目</a></li>
-        <!-- <li class="dot"></li>
-        <li><a>客户故事</a></li>
-        <li class="dot"></li>
-        <li><a>团队介绍</a></li> -->
-      </ul>
-    </div>  
+    <a class="logo">
+      <img src="https://guanyang.oss-cn-beijing.aliyuncs.com/assets/img/common/logo.png">
+    </a>
+    <ul>
+      <li class="active"><a href="/">首页</a></li>
+      <li class="dot"></li>
+      <li><a>留学申请</a></li>
+      <li class="dot"></li>
+      <li><a>单项服务</a></li>
+      <li class="dot"></li>
+      <li><a href="/feature">特色项目</a></li>
+    </ul>
+  </div> 
 </div>
 </template>
-<script>
-export default {
-  data () {
-    return {
-      msg: '洋观',
-    }
-  },
-}
-</script>
 <style lang="less">
 ul{
   margin: 0;
@@ -82,27 +69,28 @@ ul{
       }
     }
   }
-  .header-nav.second{
+  .header-nav{
     margin-bottom: 1rem;
     height: .7rem;
-    background: #fff;
-    box-shadow: 0 0.125em 0.125em 0 rgba(0,0,0,.125);
-    .header-title{
+    background: transparent;
+    box-shadow: none;
+    .header-title.second{
       height: .7rem;
-      color: #1a3a7e;
+      color: #fff;
       .logo{padding-top: 0}
       ul{
         padding-top: 0;
-        li{
-          a{
-            color: #1a3a7e;
-          }
-        }
         li.dot{
-          background: #1a3a7e;
+          background: #fff;
         }
       }
     }
+  }
+  .header-nav.first{
+    position: absolute;
+    left: 50%;
+    margin-bottom: 0;
+    margin-left: -6rem;
   }
   
 
